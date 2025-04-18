5ea342878eeac85c692aebbd3538c3b1a2ae1e4d
GitHub Action <action@github.com>
2025-04-18 01:03:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/zjcrender/webstormplugini18next/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/zjcrender/webstormplugini18next/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.zjcrender.webstormplugini18next.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/zjcrender/webstormplugini18next/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/zjcrender/webstormplugini18next/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.zjcrender.webstormplugini18next.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zjcrender.webstormplugini18next.MyBundle
+import com.github.zjcrender.webstormplugini18next.services.MyProjectService
 import javax.swing.JButton
 
 
